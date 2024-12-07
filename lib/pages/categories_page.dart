d9bc28a847d0c688a993aa8ffeb91984fb76dd11
jreycruz1126 <jreycruz1126@gmail.com>
2024-12-07 06:39:44 +0000

    trabajandoe en categorias

--- lib/pages/categories_page.dart
+++ lib/pages/categories_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CategoriesPage extends StatelessWidget {
   const CategoriesPage({super.key});
@@ -7,10 +8,35 @@ class CategoriesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Categories'),
+        centerTitle: true,
+        title: const Text(
+          'Categorías',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            context.go('/home');
+          },
+        ),
       ),
-      body: const Center(
-        child: Text('Select a Category'),
+      body: ListView.builder(
+        itemCount: 1,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const ListTile(
+              contentPadding: EdgeInsets.all(15),
+              title: Text(''),
+            ),
+          );
+        },
       ),
     );
   }
